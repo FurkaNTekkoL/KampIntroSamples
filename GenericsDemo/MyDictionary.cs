bbc35493c15d61a1c49b74c7183fc06bce94fd76
furkan tekkol <tekkol.furkan@gmail.com>
2021-09-13 16:22:41 +0300

    son

---
+++ GenericsDemo/MyDictionary.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsDemo
+{
+    class MyDictionary<T>
+    {
+        T[] _numbers;
+        T[] _tempArray;
+
+        public MyDictionary()
+        {
+            _numbers = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            _tempArray = _numbers;
+            _tempArray = new T[_numbers.Length+1];
+
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _numbers[i] = _tempArray[i];
+            }
+
+            _numbers[_numbers.Length - 1] = item;
+
+        }
+    }
+}
---
+++ GenericsDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenericsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int> numbers = new MyDictionary<int>();
+            numbers.Add(10);
+        }
+    }
+}
